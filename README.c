0385e0d305bbae131cd28867f6207925990239c4
peogfreitas <164503531+peogfreitas@users.noreply.github.com>
2025-06-16 08:36:00 -0300

    README.c

---
+++ README.c
@@ -0,0 +1,12 @@
+1º:
+  #include <stdio.h>
+
+int main() {
+  int a,b;
+  printf("Digite o primeiro número: ");
+  scanf("%d", &a);
+  printf("Digite o segundo número: ");
+  scanf("%d", &b);
+  printf("A soma dos números é: %d", a+b);
+}
+
